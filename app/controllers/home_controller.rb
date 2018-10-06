ede8125c9dbf042f79d0f75ea6ebdd55ec3b67be
Mark Hermano <hermano.mark.mh@gmail.com>
2018-10-06 19:40:22 +0800

    add bootstrap styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,17 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_tenant!, :only => [ :index ]
 
+  # set current tenant by hand by using session tenant id, very similar on how we handle the session in Alphablog
   def index
+    if current_user
+      if session[:tenant_id]
+        Tenant.set_current_tenant session[:tenant_id]
+      else
+        Tenant.set_current_tenant current_user.tenants.first
+      end
+    end
+
+    @tenant = Tenant.current_tenant
+    params[:tenant_id] = @tenant.id
   end
 end
